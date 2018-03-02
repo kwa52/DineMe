0f753995bbb5c3c46edf6955e419c4324721009d
kwa52 <kwa52@sfu.ca>
2018-03-02 12:58:17 -0800

    Add Firebase authentication

---
+++ DineMe/Controller/LogInViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  DineMe
+//
+//  Created by Kyle Wang on 2018-03-01.
+//  Copyright © 2018 Kyle Wang. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LogInViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        
+        guard let email = emailTextField.text else {fatalError()}
+        guard let password = passwordTextField.text else {fatalError()}
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                self.performSegue(withIdentifier: "goToMain", sender: self)
+            }
+        }
+        
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+    }
+}
+
--- DineMe/Controller/SignUpViewController.swift
+++ DineMe/Controller/SignUpViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +26,28 @@ class SignUpViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+    
+        guard let email = emailTextField.text else {fatalError()}
+        guard let password = passwordTextField.text else {fatalError()}
+        guard let confirmPassword = confirmPasswordTextField.text else {fatalError()}
+        
+        if password != confirmPassword {
+            print("Password are not identical")
+        } else {
+            Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                if (error != nil) {
+                    print(error!)
+                } else {
+                    
+                    self.performSegue(withIdentifier: "goToMain", sender: self)
+                    
+                }
+            }
+        }
+    }
+    
     
 
     /*
