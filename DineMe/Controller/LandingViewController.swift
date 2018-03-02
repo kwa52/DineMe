6b7bedb66f430b536e7a3758ab9998ef8b143db4
kwa52 <kwa52@sfu.ca>
2018-03-01 17:48:32 -0800

    Set up log in and sign up page and auto layout

--- DineMe/Controller/LandingViewController.swift
+++ DineMe/Controller/LandingViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LandingViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
