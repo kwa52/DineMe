66ee91d46e7599d150eceed6a148259ad36f9f08
kwa52 <kwa52@sfu.ca>
2018-03-03 19:41:32 -0800

    Add swipe and delete functionality using SwipeCellKit

--- DineMe/Controller/CategoryTableViewController.swift
+++ DineMe/Controller/CategoryTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
 class CategoryTableViewController: UITableViewController {
     
@@ -42,10 +43,11 @@ class CategoryTableViewController: UITableViewController {
 
     // ask data source to insert a cell at a row
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as! SwipeTableViewCell
         cell.textLabel?.text = categories?[indexPath.row].title
-
+        
+        cell.delegate = self
+        
         return cell
     }
     
@@ -55,6 +57,8 @@ class CategoryTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        performSegue(withIdentifier: "goToRestaurants", sender: self)
     }
     
     // Add new category
@@ -79,14 +83,20 @@ class CategoryTableViewController: UITableViewController {
             } catch {
                 print(error)
             }
-            
-            
             self.tableView.reloadData()
         }
         
         alert.addAction(action)
         
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true) {
+            alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertControllerBackgroundTapped)))
+        }
+    }
+    
+    // For dismissing UIAlert after add button pressed
+    @objc func alertControllerBackgroundTapped()
+    {
+        self.dismiss(animated: true, completion: nil)
     }
     
     //
@@ -99,13 +109,13 @@ class CategoryTableViewController: UITableViewController {
         tableView.reloadData()
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+    
+//    //Override to support conditional editing of the table view.
+//    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//         //Return false if you do not want the specified item to be editable.
+//        return true
+//    }
+    
 
     /*
     // Override to support editing the table view.
@@ -134,4 +144,38 @@ class CategoryTableViewController: UITableViewController {
     }
     */
 
+}
+
+extension CategoryTableViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: nil) { action, indexPath in
+            // handle action by updating model with deletion
+            
+            if let categoryToDelete = self.categories?[indexPath.row] {
+                do {
+                    try self.realm.write {
+                        self.realm.delete(categoryToDelete)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.expansionStyle = .destructive
+        options.transitionStyle = .border
+        return options
+    }
+    
 }
